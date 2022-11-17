bd3001b68c2107e21673c61b67b3861e92476217
Alfonso Acosta <alfonso@stellar.org>
2022-11-17 03:10:21 +0100

    See if using the gnu toolchain leads to a proper unwind

--- exp/services/soroban-rpc/internal/methods/simulate_transaction.go
+++ exp/services/soroban-rpc/internal/methods/simulate_transaction.go
@@ -24,7 +24,7 @@ import (
 #cgo darwin,amd64 LDFLAGS: -L${SRCDIR}/../../lib/preflight/target/x86_64-apple-darwin/release/ -lpreflight -ldl -lm
 #cgo darwin,arm64 LDFLAGS: -L${SRCDIR}/../../lib/preflight/target/aarch64-apple-darwin/release/ -lpreflight -ldl -lm
 #cgo linux,amd64 LDFLAGS: -L${SRCDIR}/../../lib/preflight/target/x86_64-unknown-linux-musl/release/ -L${SRCDIR}/../../lib/preflight/target/x86_64-unknown-linux-gnu/release/ -lpreflight -ldl -lm
-#cgo linux,arm64 LDFLAGS: -L${SRCDIR}/../../lib/preflight/target/aarch64-unknown-linux-musl/release/ -L${SRCDIR}/../../lib/preflight/target/x86_64-unknown-linux-gnu/release/ -lpreflight -ldl -lm
+#cgo linux,arm64 LDFLAGS: -L${SRCDIR}/../../lib/preflight/target/aarch64-unknown-linux-musl/release/ -L${SRCDIR}/../../lib/preflight/target/aarch64-unknown-linux-gnu/release/ -lpreflight -ldl -lm
 */
 import "C"
 
